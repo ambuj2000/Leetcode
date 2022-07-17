f17b72e56257c204e508e3d139e159e9f53c157c
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2022-07-17 11:02:31 +0530

    Time: 14 ms (69.05%), Space: 7.1 MB (52.71%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,29 @@
+/**
+Explanation
+maxf means the max frequency of the same character in the sliding window.
+To better understand the solution,
+you can firstly replace maxf with max(count.values()),
+Now I improve from O(26n) to O(n) using a just variable maxf.
+
+
+Complexity
+Time O(n)
+Space O(128)
+
+**/
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res = 0, maxf = 0;
+        unordered_map<int, int> count;
+        for (int i = 0; i < s.length(); ++i) {
+            maxf = max(maxf, ++count[s[i]]);
+            if (res - maxf < k)
+                res++;
+            else
+                count[s[i - res]]--;
+        }
+        return res;
+    }
+};
