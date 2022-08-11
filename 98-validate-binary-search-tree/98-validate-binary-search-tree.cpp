9271f786fdffc9237a31a9691ae9924bf5d19176
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2022-08-11 22:22:17 +0530

    Time: 19 ms (56.25%), Space: 21.6 MB (88.87%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,23 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isValidBST(TreeNode*root){
-        return help(root,LONG_MIN,LONG_MAX);
+    long compare = LONG_MIN;
+    bool ans = true;
+    void inorder(TreeNode* root)
+    {
+        if(ans == true){
+            if(root == NULL)
+                return;
+            inorder(root->left);
+            if(compare < root -> val)
+                compare = root -> val;
+            else
+                ans = false;
+            inorder(root->right);
+        }
     }
-    bool help(TreeNode* root,long min,long max) {
-       if(!root)return true;
-        if(root->val<=min || root->val>=max)return false;
-        return help(root->left,min,root->val)&& help(root->right,root->val,max);
+    bool isValidBST(TreeNode* root) 
+    {
+        if(root == NULL)
+            true;
+        inorder(root);
+        return ans;            
     }
 };
-
