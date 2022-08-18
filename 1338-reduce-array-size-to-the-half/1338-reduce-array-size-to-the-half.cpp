87a3605e6ea701fc2b3d2b4acf6a870b6fe43475
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2022-08-18 11:49:21 +0530

    Time: 623 ms (7.07%), Space: 99.4 MB (5.44%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        
+        map<int,int>mp;
+        for(auto x:arr){
+            mp[x]++;
+        }
+        multimap<int,int,greater<int>>mm;
+        for(auto it:mp){
+            mm.insert({it.second,it.first});
+        }
+        int n=arr.size();
+        int sum=n;
+        int count=0;
+        for(auto it=mm.begin();it!=mm.end();it++){
+                sum-=it->first;
+            count++;
+            if(sum<=n/2){
+                return count;
+            }
+        }
+        return count;
+    }
+};
